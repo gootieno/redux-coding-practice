fd0180e0c0321c810c5ba7dd342574dda24dbf78
gootieno <geoff.otieno7@gmail.com>
2020-06-04 01:54:48 -0700

    cleaned up some files in the tree

--- first-redux-app/src/store/configureStore.js
+++ first-redux-app/src/store/configureStore.js
@@ -1,9 +1,8 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
-import reducer from './bugs ';
+import { configureStore } from '@reduxjs/toolkit';
+import reducer from './bugs';
 
 //create store is another example of a higher order function because it takes in a function as an argument.
 
-export default function configureStore() {
-	const store = createStore(reducer, devToolsEnhancer({ trace: true }));
+export default function () {
+	return configureStore({ reducer });
 }
